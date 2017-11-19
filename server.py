04b841ff2c1edc8fa7b53acd5b44ce874b5d8042
Venil Noronha <venil.noronha@gmail.com>
2017-11-19 12:11:53 -0800

    Replaces mqtt impl with xbee (#4)

--- server.py
+++ server.py
@@ -2,35 +2,27 @@
 from gevent import monkey
 from random import *
 monkey.patch_all()
-import paho.mqtt.client as mqtt
 
 import time
 from threading import Thread
 from flask import Flask, render_template, session, request
 from flask.ext.socketio import SocketIO, emit, join_room, disconnect
+import serial
 
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 thread = None
+ser = serial.Serial('/dev/tty.usbserial-141',9600)
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-# Subscribing in on_connect() means that if we lose the connection and
-# reconnect then subscriptions will be renewed.
-    client.subscribe("hello/world")
-
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print("Topic: ", msg.topic+"\nMessage: "+str(msg.payload))
-    socketio.emit('message', {'data': 'This is data', 'time': msg.payload.decode('utf-8')}, namespace='/test')
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-
-client.connect("test.mosquitto.org", 1883, 60)
+def loop_forever():
+    read_byte = ser.read(2)
+    socketio.emit('message', {'data': 'This is data', 'time': read_byte.decode('ascii')}, namespace='/test')
+    while read_byte is not None:
+	read_byte = ser.read(2)
+        socketio.emit('message', {'data': 'This is data', 'time': read_byte.decode('ascii')}, namespace='/test')
+	print read_byte.decode("ascii")
 
 def background_stuff():
      """ python code in main.py """
@@ -57,7 +49,7 @@ def index():
         thread.start()
     return app.send_static_file('index.html')
 
-mqttThread = Thread(target=client.loop_forever)
+mqttThread = Thread(target=loop_forever)
 mqttThread.start()
 
 
