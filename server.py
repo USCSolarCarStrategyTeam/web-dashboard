26d06ea015627b9e434548d684eaac9197e7d96c
Adit Bhartia <aditbhartia@guest-wireless-upc-1604-10-120-077-132.usc.edu>
2018-04-19 18:53:55 -0700

    Modified server.py to take inputs from Serial through xBee module. Added several HTML pages with different Javascript graphs that receive input from the socket created by the Python file.

--- server.py
+++ server.py
@@ -8,7 +8,8 @@ from threading import Thread
 from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, emit, join_room, disconnect
 import serial
-
+import random
+from datetime import datetime
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'secret!'
@@ -16,22 +17,61 @@ socketio = SocketIO(app)
 thread = None
 #ser = serial.Serial('/dev/tty.usbserial-141',9600)
 
+# ser = serial.Serial(
+# 
+#    port='/dev/tty.usbserial-1412',
+#    baudrate = 9600,
+# #    parity=serial.PARITY_NONE,
+# #    stopbits=serial.STOPBITS_ONE,
+# #    bytesize=serial.EIGHTBITS,
+# #    timeout=1
+# )
+
+#data1 = ser.readline();
+
 def loop_forever():
     #read_byte = ser.read(2)
     socketio.emit('message', {'data': 'This is data', 'time': read_byte.decode('ascii')}, namespace='/test')
+    socketio.emit('message2', {'data2': 'This is data', 'time': read_byte.decode('ascii')}, namespace='/test')
     while read_byte is not None:
 	#read_byte = ser.read(2)
         socketio.emit('message', {'data': 'This is data', 'time': read_byte.decode('ascii')}, namespace='/test')
+        socketio.emit('message2', {'data2': 'This is data', 'time': read_byte.decode('ascii')}, namespace='/test')
 	print read_byte.decode("ascii")
+	
+	
 
 def background_stuff():
+
      """ python code in main.py """
      print('In background_stuff')
+     random.seed(datetime.now())
+     myList = [0,0,0,0,0];
+     counter = 0;
      while True:
-        time.sleep(1)
-        print("sleeping")
-        t = str(randint(40,90))
-        socketio.emit('message', {'data': 'This is data', 'time': t}, namespace='/test')
+     	ser = serial.Serial(port='/dev/tty.usbserial-1412',baudrate = 9600)
+     	data1 = ser.readline();
+     	#value = int(data1);
+     	counter = counter + 1;
+     	# while (counter < 25):
+#      		myList[counter] = data1;
+#      		counter= counter+1;
+        #time.sleep(0.5)
+        #print("sleeping")
+        #t = str(randint(40,90))
+        #t = myList[counter];
+        #if counter == 6: counter = 0;
+        for z in range(0, 23):
+        	data1 = ser.readline();
+        	socketio.emit('message', {'data': 'This is data', 'time': data1}, namespace='/test')
+        for z in range(0, 4):
+        	data1 = ser.readline();
+        	socketio.emit('message', {'data2': 'This is data', 'time': data1}, namespace='/test')
+        x = str(randint(60,70));
+        y = str(randint(-10,10));
+        socketio.emit('message', {'data': 'This is data', 'time': y}, namespace='/test')
+        socketio.emit('message2', {'data2': 'This is data', 'time': x}, namespace='/test')
+        
 
 @app.route('/lib/<path:path>')
 def send_js(path):
